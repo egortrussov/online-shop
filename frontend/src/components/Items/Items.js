949241b8ae0cd4e18e462bc12f8f6bb9dc46347f
Yegor Trussov <egortr2004@mail.ru>
2020-05-06 19:47:22 +0600

    get items with images functionality added

--- frontend/src/components/Items/Items.js
+++ frontend/src/components/Items/Items.js
@@ -8,29 +8,73 @@ export default class Items extends Component {
     state = {
         category: null,
         isLoading: true,
-
+        items: []
     }
 
     static contextType = AuthContext;
 
-    setCategory(category) {
-        this.setState({
-            ...this.state,
-            category
-        })
-        console.log(category)
-    }
-
     loadItems() {
         const { category } = this.state;
 
-        // ...
+        fetch(`${ this.context.proxy }/api/items/categoryItems/${ category._id }`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                let { items } = res;
+                this.setState({
+                    items: res.items,
+                    isLoading: false
+                })
+
+                res.items.forEach(item => {
+                    fetch(`${ this.context.proxy }/api/items/itemImage/${ item._id }`)
+                        .then(res => res.blob())
+                        .then(res => {
+                            let reader = new FileReader();
+                            reader.readAsDataURL(res); 
+
+                            const update = () => {
+                                this.setState({
+                                    ...this.state,
+                                    items
+                                })
+                            }
+
+                            reader.onloadend = function() {
+                                let base64data = reader.result;      
+                                for (let i = 0; i < items.length; i++) {
+                                    let currItem = items[i];
+                                    if (currItem._id === item._id) {
+                                        console.log(i)
+                                        items[i].imageData = base64data;
+                                        update()
+                                        break;
+                                    }
+                                }
+                            }
+                        })
+                })
+            }) 
+    }
+
+    setCategory(category) {
+        const currentCategory = this.state.category;
+        if (currentCategory === category)
+            return;
+
+        this.setState({
+            ...this.state,
+            category,
+            items: [],
+            isLoading: true
+        }, () => {
+            this.loadItems()
+        })
+        
     }
 
     render() {
-        const { category } = this.state;
-
-        console.log(category)
+        const { category, isLoading, items } = this.state;
 
         return (
             <div>
@@ -41,7 +85,19 @@ export default class Items extends Component {
                 { category && (
                     <h2>{ category.name }</h2>
                 ) }
-                
+                {
+                    items ? 
+                        items.map(item => {
+                            console.log(item)
+                            return (
+                                <>
+                                    { item.imageData && <img src={ item.imageData } alt=""/> }
+                                    { item.title }
+                                </>
+                            )
+                        })
+                    : <h1>Loading...</h1>
+                }
             </div>
         )
     }
