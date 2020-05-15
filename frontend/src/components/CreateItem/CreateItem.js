940c181b91d39fb06918d97bfb05132e6a8cffd9
Yegor Trussov <egortr2004@mail.ru>
2020-05-15 20:54:23 +0600

    choose category functionality on create item page added

--- frontend/src/components/CreateItem/CreateItem.js
+++ frontend/src/components/CreateItem/CreateItem.js
@@ -7,7 +7,9 @@ import { AuthContext } from '../../contexts/AuthContext'
 export default class CreateItem extends Component {
 
     state = {
-
+        categories: null,
+        currentCategory: null,
+        isLoading: true
     }
 
     constructor(props) {
@@ -18,12 +20,36 @@ export default class CreateItem extends Component {
 
     static contextType = AuthContext;
 
+    componentDidMount() {
+        fetch(`${ this.context.proxy }/api/categories/allCategories`)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    ...this.state,
+                    isLoading: false,
+                    categories: res.categories,
+                    currentCategory: res.categories[0]
+                })
+            })
+    }
+    
+    setCurrentCategory(category) {
+        this.setState({
+            ...this.state,
+            currentCategory: category
+        })
+    }
+
     createItem(e) {
         e.preventDefault()
+
+        const { currentCategory } = this.state;
         
         let formEl = this.formRef.current;
 
         let formData = new FormData(formEl);
+
+        formData.append('categoryId', currentCategory._id);
 
         fetch(`${ this.context.proxy }/api/items/createItem`, {
             method: 'POST',
@@ -39,9 +65,23 @@ export default class CreateItem extends Component {
     }
 
     render() {
+        const { categories, isLoading, currentCategory } = this.state;
+
+        if (isLoading) return (
+            <h1>Loading...</h1>
+        )
+        
         return (
             <div>
-                <form ref={ this.formRef } onSubmit={ (e) => this.createItem(e) }>
+                {
+                    categories.map(category => {
+                        return (
+                            <button onClick={ () => this.setCurrentCategory(category) }>{ category.name }</button>
+                        )
+                    })
+                }
+                <span>category: { currentCategory.name }</span>
+                <form ref={ this.formRef } onSubmit={ (e) => this.createItem(e) }>                   
                     <InputField name="title" label="title" type="text" />
                     <InputField name="description" label="description" type="textarea" />
                     <InputField name="quantity" label="quantity" type="text" />
