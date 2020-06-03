7508af8672c4e671557b1c87721f3c7c5de2d6e8
Yegor Trussov <egortr2004@mail.ru>
2020-06-03 20:59:46 +0600

    all orders page design added, bugs fixed

--- frontend/src/components/AllOrders/AllOrders.js
+++ frontend/src/components/AllOrders/AllOrders.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 import { AuthContext } from '../../contexts/AuthContext'
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom'
+
+import './css/AllOrders.css'
+
+import { formatDate } from '../../middleware/dateFormat'
 
 export default class AllOrders extends Component {
 
     state = {
         isLoading: true,
-        orders: null
+        orders: null,
+        isRedirectToOrderPage: false,
+        orderIdToRedirect: null
     }
 
     static contextType = AuthContext;
@@ -29,6 +35,7 @@ export default class AllOrders extends Component {
                 if (res.isAccessError) {
                     window.location.href = '/';
                 }
+                res.orders.reverse()
                 this.setState({
                     isLoading: false,
                     orders: res.orders
@@ -36,21 +43,42 @@ export default class AllOrders extends Component {
             })
     }
     
+    goToOrderPage(id) {
+        this.setState({
+            isRedirectToOrderPage: true,
+            orderIdToRedirect: id
+        })
+    }
 
     render() {
-        const { isLoading, orders } = this.state;
+        const { isLoading, orders, isRedirectToOrderPage, orderIdToRedirect } = this.state;
 
         if (isLoading) return (
             <h1>Loading...</h1>
         )
 
+        if (isRedirectToOrderPage) return (
+            <Redirect to={ `/order/${ orderIdToRedirect }` } />
+        )
+
         return (
-            <div>
+            <div className="orders-container">
                 {
                     orders.map(order => (
-                        <Link to={ `/order/${ order._id }` }>
-                            { order.date }
-                        </Link>
+                        <div className="order-link medium-container" onClick={ () => this.goToOrderPage(order._id) }>
+                            <div className="price">
+                                Price: 
+                                <span className="bold">
+                                    { order.totalPrice }
+                                </span> <br/>
+                                <span className="bold no-margin">
+                                    { order.items.length } items
+                                </span>
+                            </div>
+                            <div className="date">
+                                { formatDate(order.date) }
+                            </div>
+                        </div>
                     ))
                 }
             </div>
