8f08daa62af33c879bdc9089d53781c4d9d9ab80
Yegor Trussov <egortr2004@mail.ru>
2020-06-24 21:22:34 +0600

    create category functionality added

--- frontend/src/components/ManageCategories/ManageCategories.js
+++ frontend/src/components/ManageCategories/ManageCategories.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 import { AuthContext } from '../../contexts/AuthContext'
 
@@ -7,6 +7,12 @@ export default class ManageCategories extends Component {
     state = {
         isLoading: true,
         categories: []
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.formEl = createRef();
     }
 
     static contextType = AuthContext;
@@ -25,8 +31,40 @@ export default class ManageCategories extends Component {
     addCategory(e) {
         e.preventDefault();
 
+        let formEl = this.formEl.current;
+        let formData = new FormData(formEl);
         
+        let request = {
+            name: formData.get('name'),
+            description: ''
+        }
+
+        fetch(`${ this.context.proxy }/api/categories/createCategory`, {
+            method: 'POST',
+            headers: {
+                'x-auth-token': this.context.token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        })
+            .then(res => res.json()) 
+            .then(res => {
+                if (res.success) {
+                    let { categories } = this.state;
+
+                    categories.push(res.category);
+
+                    this.setState({
+                        ...this.state,
+                        categories
+                    })
+                }
+            })
     }    
+
+    deleteCategory() {
+
+    }
 
     render() {
         const { isLoading, categories } = this.state;
@@ -38,14 +76,14 @@ export default class ManageCategories extends Component {
         return (
             <div>
                 {
-                    categories.map(category => (
+                    categories.map((category, inx) => (
                         <div className="category-block">
-                            <button>delete</button>
+                            <button onClick={ () => this.deleteCategory() }>delete</button>
                             <span>{ category.name } </span>
                         </div>
                     ))
                 }
-                <form onSubmit={ (e) => this.addCategory(e) } className="add-category">
+                <form ref={ this.formEl } onSubmit={ (e) => this.addCategory(e) } className="add-category">
                     <input type="text" name="name" />
                     <button>Add</button>
                 </form>
